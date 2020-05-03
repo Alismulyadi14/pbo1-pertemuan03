96185cd5d647f2f0231905f0568b8da86f7b695e
Alismulyadi14 <62732886+Alismulyadi14@users.noreply.github.com>
2020-05-03 10:17:28 +0800

    Operator Binary Assignment

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,11 @@
+package Operator;
+
+public class OperatorBinary {
+    //operator (=) sebagai assignment/penugasan
+    // menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak di jalankan oleh program
+
+    String nama = "Alis Mulyadi";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+}
