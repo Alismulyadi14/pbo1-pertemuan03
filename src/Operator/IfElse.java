c5912901d47e1fb508e62221295a0a06401bc53c
Alismulyadi14 <62732886+Alismulyadi14@users.noreply.github.com>
2020-05-03 12:15:49 +0800

    Bersifat false 3

--- src/Operator/IfElse.java
+++ src/Operator/IfElse.java
@@ -2,13 +2,16 @@ package Operator;
 
 public class IfElse {
     public static void main(String[] args ) {
-        double totalBelanja = 25000;
-        double uangDiDompet = 50000;
+        double totalBelanja = 53000;
+        double uangDiDompet = 25000;
 
         if(uangDiDompet<totalBelanja) {
             System.out.println("uang kurang, kurangi jajan anda");
+        }else if(uangDiDompet>totalBelanja) {
+            double angsul = uangDiDompet - totalBelanja;
+            System.out.println("uang cukup, angsul : " + angsul);
         }else{
-            System.out.println("uang cukup, gas teross");
+            System.out.println("uang pas, yes");
         }
     }
 }
