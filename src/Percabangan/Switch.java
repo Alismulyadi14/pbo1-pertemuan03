bd1696af97451969f31fb220831b001c3df861b5
Alismulyadi14 <62732886+Alismulyadi14@users.noreply.github.com>
2020-05-03 16:32:29 +0800

    percakapan

--- src/Percabangan/Switch.java
+++ src/Percabangan/Switch.java
@@ -2,23 +2,28 @@ package Percabangan;
 
 public class Switch {
     public static void main(String[] args) {
-        int jalanJalan=3;
-        switch (jalanJalan){
-            case 1:
-                System.out.println("tetap semangat");
+        char nilai = 'D';
+        switch (nilai) {
+            case 'A':
+                System.out.println("Mhs   : \"ulun ada Surungan nah gasan pian\"");
+                System.out.println("Dosen : \"apa tadih \"");
                 break;
-            case 2:
-                System.out.println("menjalani hidup");
+            case 'B':
+                System.out.println("Mhs   : \"mangga, mangga apa yang mengerikan\"");
+                System.out.println("Dosen : \"manggatarrrrr !!, bujurlah\"");
                 break;
-            case 3:
-                System.out.println("meski dunia");
-                break;
-            case 4:
-                System.out.println("tidak bersahabat");
+            case 'C':
+                System.out.println("Mhs   : \"salah banarrrr\"");
+                System.out.println("Dosen : \"goong dah,apa jawaban nya\"");
+                System.out.println("Mhs   : \"MANGGAruk burit Singa hahaha\"");
                 break;
             default:
-                System.out.println("pada kita");
+                System.out.println("Dosen : \"nahh m.uluti yang tuha ngene *menceleng\"");
+                System.out.println("Mhs   : \"kaboorrr hahaha\"");
+                System.out.println("Dosen : \"awas ja ikam, ku beri nilai E\"");
                 break;
+
+
         }
     }
 }
